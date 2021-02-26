02d3b0b34519b710ff2dba1f49a1343d5e415e59
Rafael Rublez <rubleske@hotmail.com>
2021-02-26 09:28:32 -0300

    Create main.go

---
+++ main.go
@@ -0,0 +1,25 @@
+package goarea
+
+import "math"
+
+// Pi é a proporção numerica definida pela relação entre
+// o perímetro de uma circunferência e seu diâmetro
+const Pi = 3.141592657
+
+// Circ é responsável por calcular a área da circunferência
+func Circ(raio float64) float64 {
+	return math.Pow(raio, 2) * Pi
+
+}
+
+// Rect é responsável por calcular a área de um retângulo
+func Rect(base, altura float64) float64 {
+	return base * altura
+
+}
+
+// Começa com minúscula ou '_': método privado
+func _TrianguloEq(base, altura float64) float64 {
+	return (base * altura) / 2
+
+}
